46c818606076c210d0dbc770f09fc8d7d850cb0b
von Schappler <vonschappler_dev@outlook.com>
2023-04-05 15:18:36 +0000

    Complete Challenge

--- src/index.js
+++ src/index.js
@@ -5,24 +5,39 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
-import React from 'react'
-import ReactDom from 'react-dom'
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
+import React from "react";
+import ReactDom from "react-dom";
 
-const hour = new Date().getHours()
-const heading = document.querySelector('h1')
-
-if (hour > 0 && hour <=12) {
-  heading.innerHTML = 'Good Morning'
-  heading.className = 'heading morning'
-} else if (hour > 12 && hour <= 18) {
-  heading.innerHTML = 'Good Afternoon'
-  heading.className = 'heading afternoon'
-} else if (hour > 18 && hour <= 0) {
-  heading.innerHTML = "Good Evening"
-  heading.className = 'heading night'
-}
+const hour = new Date().getHours();
+console.log(hour)
+const greeting = {
+  morning: "Good Morning",
+  afternoon: "Good Afternoon",
+  evening: "Good Evening"
+};
 
 ReactDom.render(
-  <h1 className='heading morning'>Good Morning</h1>,
-  document.getElementById('root')
-)
+  <h1 id="h1" className="heading morning">
+    {greeting.morning}
+  </h1>,
+  document.getElementById("root")
+);
+const heading = document.getElementById("h1");
+
+if (hour >= 0 && hour < 12) {
+  heading.innerText = greeting.morning;
+  heading.className = "heading morning";
+} else if (hour >= 12 && hour < 18) {
+  heading.innerText = greeting.afternoon;
+  heading.className = "heading afternoon";
+} else if (hour >= 18 && hour <= 23) {
+  heading.innerText = greeting.evening;
+  heading.className = "heading evening";
+}
